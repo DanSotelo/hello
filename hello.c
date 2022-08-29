1e6d80843e3b937c171ca93aec496af826510d24
DanSotelo <dsotelo2@unl.edu>
2022-08-29 22:52:09 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Daniel Sotelo
+ *         dsotelo2@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+    printf("Hello World!\n");
+     printf("Daniel Sotelo!/n");
+     printf("Electrical Engineering/n");
+  return 0;
+}
